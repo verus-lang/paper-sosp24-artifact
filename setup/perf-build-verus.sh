6e9f968a9927a805c563fd39f5d71cc8d76a5b63
Andrea Lattuada <andrea@lattuada.me>
2024-08-20 16:47:10 +0200

    minor fix

--- setup/perf-build-verus.sh
+++ setup/perf-build-verus.sh
@@ -2,4 +2,4 @@
 cd /mydata
 git clone https://github.com/verus-lang/verus.git
 
-docker run --platform=linux/amd64 --rm -it -v .:/root/eval -w /root/eval ghcr.io/utaal/ubuntu-essentials-rust-1.76.0 /bin/bash setup/perf-build-verus-in-container.sh
+docker run --platform=linux/amd64 --rm -it -v .:/root/eval -w /root/eval ghcr.io/utaal/ubuntu-essentials-rust-1.76.0 /bin/bash verus-sosp24-artifact/setup/perf-build-verus-in-container.sh
