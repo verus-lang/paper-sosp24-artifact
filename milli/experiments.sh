69501a9c8edc7df64ea44ebfeb3ea79867f65a70
Andrea Lattuada <andrea@lattuada.me>
2024-08-21 16:25:16 +0200

    set provers number creusot

--- milli/experiments.sh
+++ milli/experiments.sh
@@ -9,11 +9,17 @@ eval $(opam env --switch=4.14.1)
 
 mkdir -p results
 
+sed -i 's/running_provers_max = ./running_provers_max = 1/' /root/.why3.conf
+
 cd linked-list
 python3 oneshot.py > ../results/linked-list-oneshot.tex
 python3 repeat.py > ../results/linked-list-repeat.csv
+
+sed -i 's/running_provers_max = ./running_provers_max = 8/' /root/.why3.conf
 python3 errors.py > ../results/linked-list-errors.csv
 cd ..
+
+sed -i 's/running_provers_max = ./running_provers_max = 1/' /root/.why3.conf
 
 cd doubly-linked-list
 python3 oneshot.py > ../results/doubly-linked-list-oneshot.tex
